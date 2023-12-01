39f650e729a048b9832ec32ae4dc3c1379bf199d
Vaxied <psgmarcelo@gmail.com>
2023-12-01 00:18:19 -0400

    Updates

---
+++ assets/secret.js
@@ -0,0 +1,4 @@
+const apiKey = 'fa2206770fmsh314dcad19d79111p177eb2jsn091ea22a3206'
+const apiHost = 'youtube-v311.p.rapidapi.com'
+const apiAddress = 'https://youtube-v311.p.rapidapi.com/activities/?part=snippet&channelId=UCmkkEZtKJr5CA4v4218yUHQ&maxResults=5'
+export { apiKey, apiHost, apiAddress }
